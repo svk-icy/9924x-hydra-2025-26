967490974c7c2e520719197465bda5fabd856651
not_shiv <151963917+shivy-alt@users.noreply.github.com>
2025-08-05 21:53:00 -0700

    9924x-2025-26

---
+++ include/subsystems.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "EZ-Template/api.hpp"
+#include "api.h"
+
+extern Drive chassis;
+
+// Your motors, sensors, etc. should go here.  Below are examples
+
+// inline pros::Motor intake(1);
+// inline pros::adi::DigitalIn limit_switch('A');
+inline pros::Motor intake_motor1(8);
+inline pros::Motor intake_motor2(1);
